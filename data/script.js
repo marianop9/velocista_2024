f168afa3631a9adbee698bbcfb840803d90f1680
Mariano Perez <perezm7401@gmail.com>
2024-09-20 12:14:12 -0300

    pid-controller ui: real-time erorr graph

--- data/script.js
+++ data/script.js
@@ -1,7 +1,11 @@
 window.onload = () => {
+  // obtiene los valores iniciales de cada parametro
   getParam(PIDParams.Kp);
   getParam(PIDParams.Kd);
   getParam(PIDParams.Ki);
+
+  // por defecto no grafica el error
+  document.getElementById('pollCheckbox').checked = false;
 }
 
 const baseUrl = 'http://192.168.0.143'
@@ -20,8 +24,10 @@ const PIDParams = {
   Kd: 'kd'
 }
 
+// clase para los botones mientras se ejecuta el request
 const classIsLoading = 'is-loading';
 
+// request GET para recuperar un parametro
 function getParam(param) {
   fetch(getParamUrl(param))
     .then((async (resp) => {
@@ -31,22 +37,24 @@ function getParam(param) {
     .catch(console.error);
 }
 
+// request POST para asignar un parametro
 function setParam(param, value) {
   const buttonName = "btnSubmit-" + param;
   document.getElementById(buttonName).classList.add(classIsLoading);
-  
-  fetch(setParamUrl(param, value), {method: "POST"})
-  .then(async (resp) => {
-    const value = await resp.text();
-    document.getElementById(param).innerHTML = value;
-    console.log(value);        
-  })  
-  .catch(console.error)
-  .finally(() => {
-    document.getElementById(buttonName).classList.remove(classIsLoading);
-  });
+
+  fetch(setParamUrl(param, value), { method: "POST" })
+    .then(async (resp) => {
+      const value = await resp.text();
+      document.getElementById(param).innerHTML = value;
+      console.log(value);
+    })
+    .catch(console.error)
+    .finally(() => {
+      document.getElementById(buttonName).classList.remove(classIsLoading);
+    });
 }
 
+// valida el valor de cada parametro
 function validateValue(value) {
   if (!value || isNaN(value)) {
     alert("un numerito pls");
@@ -59,6 +67,7 @@ function validateValue(value) {
   return true;
 }
 
+// formulario KP
 const kpForm = document.getElementById("formKp");
 kpForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
@@ -66,12 +75,13 @@ kpForm.addEventListener("submit", (evt) => {
   const formData = new FormData(kpForm);
 
   const value = formData.get("value");
-  if (!validateValue(value)) 
+  if (!validateValue(value))
     return;
-  
+
   setParam(PIDParams.Kp, value);
 })
 
+// formulario KI
 const kiForm = document.getElementById("formKi");
 kiForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
@@ -79,12 +89,13 @@ kiForm.addEventListener("submit", (evt) => {
   const formData = new FormData(kiForm);
 
   const value = formData.get("value");
-  if (!validateValue(value)) 
+  if (!validateValue(value))
     return;
-    
+
   setParam(PIDParams.Ki, value);
 })
 
+// formulario KD
 const kdForm = document.getElementById("formKd");
 kdForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
@@ -92,86 +103,70 @@ kdForm.addEventListener("submit", (evt) => {
   const formData = new FormData(kdForm);
 
   const value = formData.get("value");
-  if (!validateValue(value)) 
+  if (!validateValue(value))
     return;
-  
+
   setParam(PIDParams.Kd, value);
 })
 
-function getKp(evt) {
-    // var xhttp = new XMLHttpRequest();
-    // xhttp.onreadystatechange = function() {
-    //   if (this.readyState == 4 && this.status == 200) {
-    //     document.getElementById("Kp").innerHTML =
-    //     this.responseText;
-    //   }
-    // };
-    // xhttp.open("GET", "/getKp", true);
-    // xhttp.send();
-    
-  }
-  
-  function getKi() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("Ki").innerHTML =
-        this.responseText;
+// grafico del error en tiempo real
+// se inicializa vacio, sin datos
+const ctx = document.getElementById("errorChart");
+const chart = new Chart(ctx, {
+  type: 'line',
+  options: {
+    animation: false,
+  },
+  data: {
+    labels: [],
+    datasets: [{
+      label: 'Error',
+      data: [],
+      fill: false,
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0.1
+    }]
+  },
+});
+
+// recupera el error y lo agrega al grafico
+function fetchError() {
+  if (!chart)
+    return;
+
+  fetch(baseUrl + '/getError')
+    .then(async (resp) => {
+      // recupera el valor
+      const error = await resp.text();
+
+      // siempre deberia ser un numero
+      if (isNaN(error)) {
+        console.error('getError response - got: ' + error);
+        return;
       }
-    };
-    xhttp.open("GET", "/getKi", true);
-    xhttp.send();
+
+      // recupera la fecha actual (mm:ss) y lo agrega al eje x
+      const date = new Date();
+      const label = `${date.getMinutes()}:${date.getSeconds()}`;
+      chart.data.labels.push(label);
+
+      // agrega el valor del error al eje y
+      chart.data.datasets[0].data.push(error);
+
+      chart.update();
+    })
+    .catch(console.error);
+}
+
+// checkbox que controla la actualizacion del grafico del error
+let intervalId;
+document.getElementById('pollCheckbox').addEventListener('change', (evt) => {
+  const shouldPoll = evt.target.checked;
+
+  if (shouldPoll) {
+    intervalId = setInterval(fetchError, 2000);
+  } else {
+    clearInterval(intervalId);
+    intervalId = null;
   }
-  
-  function getKd() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("Kd").innerHTML =
-        this.responseText;
-      }
-    };
-    xhttp.open("GET", "/getKd", true);
-    xhttp.send();
-  }
-  
-  // var chartT = new Highcharts.Chart({
-  //   chart:{ renderTo : 'chart-temperature' },
-  //   title: { text: 'PID Error' },
-  //   series: [{
-  //     showInLegend: false,
-  //     data: []
-  //   }],
-  //   plotOptions: {
-  //     line: { animation: false,
-  //       dataLabels: { enabled: true }
-  //     },
-  //     series: { color: '#059e8a' }
-  //   },
-  //   xAxis: { type: 'datetime',
-  //     dateTimeLabelFormats: { second: '%H:%M:%S' }
-  //   },
-  //   yAxis: {
-  //     title: { text: 'Error' }
-  //     //title: { text: 'Temperature (Fahrenheit)' }
-  //   },
-  //   credits: { enabled: false }
-  // });
-  
-  // setInterval(function( ) {
-  //   var xhttp = new XMLHttpRequest();
-  //   xhttp.onreadystatechange = function() {
-  //     if (this.readyState == 4 && this.status == 200) {
-  //       var x = (new Date()).getTime(),
-  //           y = parseFloat(this.responseText);
-  //       //console.log(this.responseText);
-  //       if(chartT.series[0].data.length > 40) {
-  //         chartT.series[0].addPoint([x, y], true, true, true);
-  //       } else {
-  //         chartT.series[0].addPoint([x, y], true, false, true);
-  //       }
-  //     }
-  //   };
-  //   xhttp.open("GET", "/error", true);
-  //   xhttp.send();
-  // }, 1000 ) ;
+})
