05ae72e3af4671a489128dd1af9ee18bc3da4511
Mariano Perez <perezm7401@gmail.com>
2024-09-19 21:57:06 -0300

    renamed api endpoints

--- lib/pid_tuner/pid_tuner.cpp
+++ lib/pid_tuner/pid_tuner.cpp
@@ -34,7 +34,7 @@ void pid_tuner_init(const char *ssid, const char *pwd) {
 }
 
 void pid_tuner_set_endpoints() {
-    server.on("/", [](AsyncWebServerRequest *request) {
+    server.on("/greet", [](AsyncWebServerRequest *request) {
         request->send(200, "text/plain", "Heello world!");
     });
 
@@ -42,17 +42,17 @@ void pid_tuner_set_endpoints() {
         request->send(SPIFFS, "/greeting.txt", "text/plain");
     });
 
-    server.on("/index.html", [](AsyncWebServerRequest *request) {
+    server.on("/", [](AsyncWebServerRequest *request) {
         request->send(SPIFFS, "/index.html", "text/html");
-    });
-
-    server.on("/style.css", [](AsyncWebServerRequest *request) {
-        request->send(SPIFFS, "/style.css", "text/css");
     });
 
     server.on("/script.js", [](AsyncWebServerRequest *request) {
         request->send(SPIFFS, "/script.js", "text/javascript");
     });
+
+    // server.on("/style.css", [](AsyncWebServerRequest *request) {
+    //     request->send(SPIFFS, "/style.css", "text/css");
+    // });
 
     server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request) {
         if (!request->hasParam("param")) return;
